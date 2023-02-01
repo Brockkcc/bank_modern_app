961c1273aa529fd665810c1fd6b39c5213c87200
Brock Carless <brock.carless@gmail.com>
2023-02-01 10:40:16 -0800

    Hero Section

--- src/App.jsx
+++ src/App.jsx
@@ -4,20 +4,20 @@ import React from 'react';
 import { Navbar, Hero, Stats, Business, Billing, CardDeal, Testimonials, Clients, CTA, Footer } from './components';
 
 const App = () =>( 
-    <div className="bg-black-gradient w-full overflow-hidden">
+    <div className="bg-black w-full overflow-hidden">
       <div className={`${styles.paddingX} ${styles.flexCenter}`}>
         <div className={`${styles.boxWidth}`}>
           <Navbar />
         </div>
       </div>
 
-      <div className={`bg-blue-gradient ${styles.flexStart}`}>
+      <div className={`bg-black ${styles.flexStart}`}>
         <div className={`${styles.boxWidth}`}>
          <Hero />
         </div>
       </div>
 
-      <div className={`bg-black-gradient ${styles.paddingX} ${styles.flexStart}`}>
+      <div className={`bg-black ${styles.paddingX} ${styles.flexStart}`}>
         <div className={`${styles.boxWidth}`}>
          <Stats />
          <Business />
